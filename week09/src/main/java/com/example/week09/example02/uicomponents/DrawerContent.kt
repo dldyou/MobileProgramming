50e37850c16b1ea7a529c1dd6f749b1820c36399
dldyou <chan12_yun@naver.com>
2025-05-12 13:03:29 +0900

    .

--- week09/src/main/java/com/example/week09/example02/uicomponents/DrawerContent.kt
+++ week09/src/main/java/com/example/week09/example02/uicomponents/DrawerContent.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Icon
@@ -16,6 +17,7 @@ import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,6 +37,7 @@ fun ColumnScope.DrawerContent() {
             modifier = Modifier
                 .width(40.dp)
                 .height(40.dp)
+                .clip(CircleShape)
         )
     }
 
